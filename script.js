4a74fc03c5045d3d82e824a1c73d425ccfc35859
JacobBersheba89 <jakubpawlas89@gmail.com>
2024-10-26 16:48:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function getWeather() {
-    const city = document.getElementById('cityInput').value;
+function getForecast(city) {
     const apiKey = '7ead51f4d43b3ee0ac451738a3ea079d';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=cz`;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=cz`;
 
     fetch(url)
         .then(response => {
@@ -11,21 +10,41 @@ function getWeather() {
             return response.json();
         })
         .then(data => {
-            displayWeather(data);
+            displayForecast(data);
         })
         .catch(error => {
             console.error('There has been a problem with your fetch operation:', error);
         });
 }
 
+function displayForecast(data) {
+    let forecastHtml = '';
+    data.list.forEach((item, index) => {
+        if (index % 8 === 0) { // Každý den má 8 intervalů po 3 hodinách
+            forecastHtml += `
+                <div class="dayForecast">
+                    <h3>${new Date(item.dt * 1000).toLocaleDateString()}</h3>
+                    <p>Průměrná teplota: ${item.main.temp} °C</p>
+                    <p>${item.weather[0].description}</p>
+                    <p>Vlhkost: ${item.main.humidity}%</p>
+                </div>
+            `;
+        }
+    });
+    document.getElementById('forecastHeader').style.display = 'block'; // Zobrazí nadpis
+    document.getElementById('forecastWeatherResult').style.display = 'flex'; // Zobrazí předpověď
+    document.getElementById('forecastWeatherResult').innerHTML = forecastHtml;
+}
+
 function displayWeather(data) {
     const weather = `
-        <h2>Počasí ve městě: ${data.name}</h2>
+        <h2>Aktuální počasí ve městě: ${data.name}</h2>
         <p>Teplota: ${data.main.temp} °C</p>
         <p>Aktuální situace: ${data.weather[0].description}</p>
         <p>Vlhkost: ${data.main.humidity}%</p>
     `;
-    document.getElementById('weatherResult').innerHTML = weather;
+    document.getElementById('currentWeatherResult').innerHTML = weather;
+    document.getElementById('currentWeatherResult').style.display = 'block'; // Zobrazí výsledek
 }
 
 function getWeather() {
@@ -48,3 +67,4 @@ function getWeather() {
             console.error('There has been a problem with your fetch operation:', error);
         });
 }
+
